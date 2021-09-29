506eeb5dfd2ade2409204830980bc99e13910ae5
unknown <erickaugs@gmail.com>
2021-09-29 16:18:14 -0300

    prog

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String j;
+        int z;
+        double y;
+        j = sc.next();
+        z = sc.nextInt();
+        y = sc.nextDouble();
+        System.out.println("Voce digitou: " + j);
+        System.out.println("Voce digitou: " + z);
+        System.out.println("Voce digitou: " + y);
+
+    }
+}
